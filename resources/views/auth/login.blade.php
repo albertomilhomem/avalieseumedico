7e765f8998d1fba024f1658eeea4104ad0fc7fb2
Alberto Milhomem <alberto.milho@gmail.com>
2016-02-12 21:37:30 -0400

    Alteração na página de login, tradução de algumas palavras e link para a página de registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,51 +4,52 @@
 @endsection
 
 @section('content')
-	<div class="container">
-		<div class="col-sm-offset-2 col-sm-8">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					Login
-				</div>
+<div class="container">
+	<div class="col-sm-offset-2 col-sm-8">
 
-				<div class="panel-body">
-					<!-- Display Validation Errors -->
-					@include('common.errors')
+	<p class="text-center">Não é registrado ainda? Registre-se <a href="{{ action('Auth\AuthController@getRegister') }}">aqui</a> </p>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				Área de Login
+			</div>
 
+			<div class="panel-body">
+				<!-- Display Validation Errors -->
+				@include('common.errors')
 
-					<!-- New Task Form -->
-					<form action="{{ action('Auth\AuthController@postLogin') }}" method="POST" class="form-horizontal">
-						{{ csrf_field() }}
+				<!-- New Task Form -->
+				<form action="{{ action('Auth\AuthController@postLogin') }}" method="POST" class="form-horizontal">
+					{{ csrf_field() }}
 
-						<!-- E-Mail Address -->
-						<div class="form-group">
-							<label for="email" class="col-sm-3 control-label">E-mail</label>
+					<!-- E-Mail Address -->
+					<div class="form-group">
+						<label for="email" class="col-sm-3 control-label">E-mail</label>
 
-							<div class="col-sm-6">
-								<input type="email" name="email" class="form-control" value="{{ old('email') }}">
-							</div>
+						<div class="col-sm-6">
+							<input type="email" name="email" class="form-control" value="{{ old('email') }}">
 						</div>
+					</div>
 
-						<!-- Password -->
-						<div class="form-group">
-							<label for="password" class="col-sm-3 control-label">Password</label>
+					<!-- Password -->
+					<div class="form-group">
+						<label for="password" class="col-sm-3 control-label">Senha</label>
 
-							<div class="col-sm-6">
-								<input type="password" name="password" class="form-control">
-							</div>
+						<div class="col-sm-6">
+							<input type="password" name="password" class="form-control">
 						</div>
+					</div>
 
-						<!-- Login Button -->
-						<div class="form-group">
-							<div class="col-sm-offset-3 col-sm-6">
-								<button type="submit" class="btn btn-default">
-									<i class="fa fa-btn fa-sign-in"></i>Login
-								</button>
-							</div>
+					<!-- Login Button -->
+					<div class="form-group">
+						<div class="col-sm-offset-3 col-sm-6">
+							<button type="submit" class="btn btn-default">
+								<i class="fa fa-btn fa-sign-in"></i> Logar
+							</button>
 						</div>
-					</form>
-				</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
+</div>
 @endsection
