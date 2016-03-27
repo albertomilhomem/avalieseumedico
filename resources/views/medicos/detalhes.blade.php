dfb50ca00eb6fc02ba8400b287c83b25aeb72fc0
Alberto Milhomem <alberto.milho@gmail.com>
2016-03-26 23:45:43 -0400

    Correção da página de detalhes do médico: adição do botão para avaliar

--- resources/views/medicos/detalhes.blade.php
+++ resources/views/medicos/detalhes.blade.php
@@ -27,8 +27,10 @@
 					<img alt="User Pic" src="
 					http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" class="img-circle img-responsive">
 					@endif
-
 					<br>
+					@if(!Auth::guest())		
+					<a href="{{ action('AvaliacaoController@create', $medico->id) }}" class="btn btn btn-info"><i class="fa fa-comments"></i> Avaliar</a>
+					@endif
 				</div>
 				<div class=" col-md-9 col-lg-9 "> 
 					<table class="table table-user-information">
@@ -45,40 +47,40 @@
 									{{$medico->especialidade->nome}}
 								</td>
 							</tr>
-						</tbody>
-					</table>
+							</tbody>
+						</table>
+					</div>
 				</div>
 			</div>
-		</div>
-	</div>	
-</div>
-
-
-<div class="col-sm-4 col-sm-offset-4">
-	<h3 class="text-center">Avaliações <span class="badge text-left">{{ count($medico->avaliacoes)}}</span></h3>
-	@if(count($medico->avaliacoes) > 0)
-	@foreach($medico->avaliacoes as $avaliacao)			
-	<div class="panel panel-default">
-		<div class="panel-body">
-			<div class="media">
-				@include('medicos.avaliacao')
-			</div>
-
-			@if (!Auth::guest())
-			@if (Auth::user()->id == $avaliacao->user->id)
-			<div class="col-sm-6 col-sm-offset-3">					
-				<a href="{{ action('Usuario\AvaliacaoController@alterar', $avaliacao->id) }}" class="btn btn-primary"><i class="fa fa-pencil-square-o"></i> Alterar</a>
-				<a href="{{ action('Usuario\AvaliacaoController@deletar', $avaliacao->id) }}" class="btn btn-danger"><i class="fa fa-trash"></i> Excluir</a>
-			</div>
-			@endif
-			@endif
-		</div>
+		</div>	
 	</div>
 
-	@endforeach
-	@else
-	<div class="well">Esse médico não possui avaliações.</div>
-	@endif
-</div>	
 
-@endsection
+	<div class="col-sm-4 col-sm-offset-4">
+		<h3 class="text-center">Avaliações <span class="badge text-left">{{ count($medico->avaliacoes)}}</span></h3>
+		@if(count($medico->avaliacoes) > 0)
+		@foreach($medico->avaliacoes as $avaliacao)			
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<div class="media">
+					@include('medicos.avaliacao')
+				</div>
+
+				@if (!Auth::guest())
+				@if (Auth::user()->id == $avaliacao->user->id)
+				<div class="col-sm-6 col-sm-offset-3">					
+					<a href="{{ action('Usuario\AvaliacaoController@alterar', $avaliacao->id) }}" class="btn btn-primary"><i class="fa fa-pencil-square-o"></i> Alterar</a>
+					<a href="{{ action('Usuario\AvaliacaoController@deletar', $avaliacao->id) }}" class="btn btn-danger"><i class="fa fa-trash"></i> Excluir</a>
+				</div>
+				@endif
+				@endif
+			</div>
+		</div>
+
+		@endforeach
+		@else
+		<div class="well">Esse médico não possui avaliações.</div>
+		@endif
+	</div>	
+
+	@endsection
